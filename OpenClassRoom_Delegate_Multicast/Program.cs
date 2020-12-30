b628667b524fb1fc9da894b1f86131b8b062a6f1
Guillaume Baptist <guillaume.baptist@free.fr>
2020-12-30 21:30:17 +0100

    Ajoutez des fichiers projet.

---
+++ OpenClassRoom_Delegate_Multicast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenClassRoom_Delegate_Multicast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
