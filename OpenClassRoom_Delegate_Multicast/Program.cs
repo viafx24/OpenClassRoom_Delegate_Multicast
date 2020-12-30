271b677c45418b82074fee5cd9742577466c5814
Guillaume Baptist <guillaume.baptist@free.fr>
2020-12-30 21:34:21 +0100

    exemple delegate et mutlicast

--- OpenClassRoom_Delegate_Multicast/Program.cs
+++ OpenClassRoom_Delegate_Multicast/Program.cs
@@ -6,7 +6,40 @@ namespace OpenClassRoom_Delegate_Multicast
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tableau = new int[] { 4, 1, 6, 10, 8, 5 };
+            new TrieurDeTableau().DemoTri(tableau);
+        }
+    }
+
+    public class TrieurDeTableau
+    {
+        private delegate void DelegateTri(int[] tableau);
+
+        private void TriAscendant(int[] tableau)
+        {
+            Array.Sort(tableau);
+            foreach (int i in tableau)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+
+        private void TriDescendant(int[] tableau)
+        {
+            Array.Sort(tableau);
+            Array.Reverse(tableau);
+            foreach (int i in tableau)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void DemoTri(int[] tableau)
+        {
+            DelegateTri tri = TriAscendant;
+            tri += TriDescendant;
+            tri(tableau);
         }
     }
 }
